aa933a3d846eea0884bda6d71bd102450ecd48c6
Rishabh Gist <damyrachit@gmail.com>
2022-12-02 13:19:03 +0530

    added implementation for ICustomerService Interface

--- src/main/java/com/niit/customer/service/CustomerService.java
+++ src/main/java/com/niit/customer/service/CustomerService.java
@@ -1,4 +1,22 @@
 package com.niit.customer.service;
 
-public class CustomerService {
+import com.niit.customer.domain.Customer;
+
+import java.util.List;
+
+public class CustomerService implements ICustomerService{
+    @Override
+    public Customer save(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        return null;
+    }
+
+    @Override
+    public Customer getByID(int id) {
+        return null;
+    }
 }

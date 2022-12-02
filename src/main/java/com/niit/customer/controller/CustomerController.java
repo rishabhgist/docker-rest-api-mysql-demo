e6e575a157b7b307d6cca15ba500b1358f80a12a
Rishabh Gist <damyrachit@gmail.com>
2022-12-02 13:27:06 +0530

    added PostMapping to save

--- src/main/java/com/niit/customer/controller/CustomerController.java
+++ src/main/java/com/niit/customer/controller/CustomerController.java
@@ -1,13 +1,11 @@
 package com.niit.customer.controller;
 
+import com.niit.customer.domain.Customer;
 import com.niit.customer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,5 +23,9 @@ public class CustomerController {
     @GetMapping("/customer/{id}")
     public ResponseEntity<?> getById(@PathVariable int id){
         return new ResponseEntity<>(customerService.getByID(id), HttpStatus.OK);
+    }
+    @PostMapping("/post")
+    public ResponseEntity<?> save(@RequestBody Customer customer){
+        return new ResponseEntity<>(customerService.save(customer), HttpStatus.CREATED);
     }
 }

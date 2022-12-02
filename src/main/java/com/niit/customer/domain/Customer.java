7ceab4ea3a95b7975167e57e76f6c0d7969f0f32
Rishabh Gist <damyrachit@gmail.com>
2022-12-02 13:35:45 +0530

    clean, compile and package the project

--- src/main/java/com/niit/customer/domain/Customer.java
+++ src/main/java/com/niit/customer/domain/Customer.java
@@ -1,11 +1,13 @@
 package com.niit.customer.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Customer {
     @Id
+    @GeneratedValue
     private int id;
     private String firstname;
     private String lastname;

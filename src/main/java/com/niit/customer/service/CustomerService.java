e902bfc1c4fa16dff906a41a15e062410946fc6c
Rishabh Gist <damyrachit@gmail.com>
2022-12-02 13:18:20 +0530

    added CustomerService class

---
+++ src/main/java/com/niit/customer/service/CustomerService.java
@@ -0,0 +1,4 @@
+package com.niit.customer.service;
+
+public class CustomerService {
+}
--- src/main/java/com/niit/customer/service/ICustomerService.java
+++ src/main/java/com/niit/customer/service/ICustomerService.java
@@ -1,4 +1,11 @@
 package com.niit.customer.service;
 
+import com.niit.customer.domain.Customer;
+
+import java.util.List;
+
 public interface ICustomerService {
+    Customer save(Customer customer);
+    List<Customer> getAll();
+    Customer getByID(int id);
 }

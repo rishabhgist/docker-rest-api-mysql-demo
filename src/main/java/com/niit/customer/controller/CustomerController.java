1b2d8c083434bf460658cf76d2e2aab8875c23f6
Rishabh Gist <damyrachit@gmail.com>
2022-12-02 13:23:15 +0530

    added RequestMapping and RestController annotations

--- src/main/java/com/niit/customer/controller/CustomerController.java
+++ src/main/java/com/niit/customer/controller/CustomerController.java
@@ -1,10 +1,16 @@
 package com.niit.customer.controller;
 
 import com.niit.customer.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1")
 public class CustomerController {
     private CustomerService customerService;
 
+    @Autowired
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }

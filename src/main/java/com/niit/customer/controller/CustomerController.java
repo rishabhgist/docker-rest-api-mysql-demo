67b8d23b5c61205ac7e66a195671f39af8496869
Rishabh Gist <damyrachit@gmail.com>
2022-12-02 13:22:36 +0530

    added CustomerService object and initialized it

--- src/main/java/com/niit/customer/controller/CustomerController.java
+++ src/main/java/com/niit/customer/controller/CustomerController.java
@@ -1,4 +1,11 @@
 package com.niit.customer.controller;
 
+import com.niit.customer.service.CustomerService;
+
 public class CustomerController {
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 }

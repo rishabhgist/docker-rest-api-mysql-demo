7f7991d7ecdfd30a3ad1c1c24d11ef5191b3de08
Rishabh Gist <damyrachit@gmail.com>
2022-12-02 13:20:00 +0530

    added CustomerRepository and initialized with constructor

--- src/main/java/com/niit/customer/service/CustomerService.java
+++ src/main/java/com/niit/customer/service/CustomerService.java
@@ -1,10 +1,19 @@
 package com.niit.customer.service;
 
 import com.niit.customer.domain.Customer;
+import com.niit.customer.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class CustomerService implements ICustomerService{
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     @Override
     public Customer save(Customer customer) {
         return null;

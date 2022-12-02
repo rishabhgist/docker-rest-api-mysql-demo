c4ec756573b0ce379ed17e703bd665080f917553
Rishabh Gist <damyrachit@gmail.com>
2022-12-02 13:12:26 +0530

    added fields in customer class

--- src/main/java/com/niit/customer/domain/Customer.java
+++ src/main/java/com/niit/customer/domain/Customer.java
@@ -1,4 +1,10 @@
 package com.niit.customer.domain;
 
 public class Customer {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String address;
+    private long phone;
 }

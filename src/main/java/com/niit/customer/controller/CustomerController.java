f5aaf69fdcbc8548c8134fede5875d35d4b52295
Rishabh Gist <damyrachit@gmail.com>
2022-12-02 13:25:40 +0530

    added GetMapping for ID

--- src/main/java/com/niit/customer/controller/CustomerController.java
+++ src/main/java/com/niit/customer/controller/CustomerController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,12 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
-    @GetMapping("customers")
+    @GetMapping("/customers")
     public ResponseEntity<?> getAll(){
         return new ResponseEntity<>(customerService.getAll(), HttpStatus.OK);
     }
-
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<?> getById(@PathVariable int id){
+        return new ResponseEntity<>(customerService.getByID(id), HttpStatus.OK);
+    }
 }

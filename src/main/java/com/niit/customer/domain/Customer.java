74b279e76ef11f1d0afe55832d9d23f7688420d1
Rishabh Gist <damyrachit@gmail.com>
2022-12-02 13:13:27 +0530

    added getters and setters

--- src/main/java/com/niit/customer/domain/Customer.java
+++ src/main/java/com/niit/customer/domain/Customer.java
@@ -11,12 +11,60 @@ public class Customer {
     public Customer() {
     }
 
-    public Customer(int id, String firstname, String lastname, String email, String address, long phone) {a
+    public Customer(int id, String firstname, String lastname, String email, String address, long phone) {
         this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
         this.address = address;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
         this.phone = phone;
     }
 }
